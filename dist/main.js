2f9737411960120288c4c0ae9ff67d09bb8efff5
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-10 10:43:56 -0800

    Setup game variables

--- dist/main.js
+++ dist/main.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("console.log(\"webpack is working\");\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("console.log(\"webpack is working\");\n\nlet canvas,\n  ctx,\n  prevX = 0,\n  currX = 0,\n  prevY = 0,\n  currY = 0,\n  lineColor = '#008080',\n  lineSize = 2;\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log("webpack is working");
+
+let canvas,
+  ctx,
+  prevX = 0,
+  currX = 0,
+  prevY = 0,
+  currY = 0,
+  lineColor = '#008080',
+  lineSize = 2;
+
