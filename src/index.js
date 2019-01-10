223ac984f55d6f6550e79e0158672e3faee025f6
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-10 10:49:15 -0800

    Write init function

--- src/index.js
+++ src/index.js
@@ -7,5 +7,17 @@ let canvas,
   prevY = 0,
   currY = 0,
   lineColor = '#008080',
-  lineSize = 2;
+  lineSize = 2,
+  width,
+  height;
 
+function init(){
+  canvas = document.getElementById('game-canvas');
+  ctx = canvas.getContext('2d');
+  width = canvas.width;
+  height = canvas.height;
+
+  canvas.addEventListener('mousemove', e => {
+    findxy('move', e)
+  });
+}
