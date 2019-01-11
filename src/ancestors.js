52eaa5a2f7b8b0c93ffff8f6bef1e92a34eaa74e
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-11 07:37:01 -0800

    Create classes that will be extended by other classes

---
+++ src/ancestors.js
@@ -0,0 +1,57 @@
+export class Point {
+
+  constructor(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+  }
+
+  distanceTo(p) {
+    var dx = p.x - this.x;
+    var dy = p.y - this.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  clonePosition() {
+    return { x: this.x, y: this.y };
+  }
+
+  interpolate(x, y, amp) {
+    this.x += (x - this.x) * amp;
+    this.y += (y - this.y) * amp;
+  }
+}
+
+export class Entity extends Point{
+  constructor(x, y){
+    super(x, y);
+    this.alive = false;
+  }
+}
+
+export class Multiplier {
+  constructor(step, max){
+    this.major = 1;
+    this.minor = 0;
+
+    this.max = max;
+    this.step = step;
+  }
+
+  reset(){
+    this.major = 1;
+    this.minor = 0;
+  }
+
+  increase() {
+    this.minor += this.step;
+
+    // Do we need to increment the major value?
+    while (this.minor >= 1) {
+      if (this.major < this.max) {
+        this.major++;
+      }
+
+      this.minor = 1 - this.minor;
+    }
+  }
+}
